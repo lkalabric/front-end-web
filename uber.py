dfbd1d2217ccd008a0c56e69a66da829bd1252d0
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-10-14 13:37:38 -0500

    Update uber.py

--- uber.py
+++ uber.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+st.title('Uber pickups in Salvador')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
